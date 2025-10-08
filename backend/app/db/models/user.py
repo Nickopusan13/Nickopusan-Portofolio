85c9c4a388a9527cf9216e72d631325963328025
Nickopusan <nickowork13@gmail.com>
2025-10-09 01:20:21 +0700

    updated

--- backend/app/db/models/user.py
+++ backend/app/db/models/user.py
@@ -2,7 +2,7 @@ from sqlalchemy import Column, String, Integer, DateTime, func
 from app.db.base import Base
 
 class User(Base):
-    __tablename__ = "user"
+    __tablename__ = "users"
     id = Column(Integer, primary_key=True)
     name = Column(String(155), nullable=False)
     email = Column(String(155), nullable=False)
--- backend/app/routes/user.py
+++ backend/app/routes/user.py
@@ -3,7 +3,6 @@ from app.db.session import AsyncSession
 from app.db.dependencies import get_db
 from app.schemas.user import UserMessage
 from app.crud.user import create_message
-import os
 
 router = APIRouter()
 
@@ -17,17 +16,6 @@ async def user_message(data: UserMessage, db: AsyncSession = Depends(get_db)):
             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
             detail=f"Error {e}"
         )
-
-@router.get("/debug-cors")
-async def debug_cors():
-    # This will print the value to your Vercel runtime logs
-    origins = os.getenv("ALLOWED_ORIGINS")
-    print(f"DEBUG: ALLOWED_ORIGINS value is: '{origins}'")
-    
-    if origins:
-        # We can also return it to see it in the browser
-        return {"allowed_origins_from_env": origins.split(",")}
-    return {"error": "ALLOWED_ORIGINS is not set"}
 
 @router.get("/api/hello")
 def read_root():
